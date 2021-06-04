819df3da36ec45571da25c0ca9574979c5d5ecdc
Kevin Alves <kevinalves@hotmail.fr>
2021-06-04 09:33:54 +0200

    MAJ Forum 04/06/21

---
+++ structs.go
@@ -0,0 +1,62 @@
+package main
+
+import "time"
+
+type User struct {
+	ID       int
+	Username string
+	Password string
+	Email    string
+	Avatar   string
+}
+
+type Comment struct {
+	ID       int
+	UserID   int
+	PostID   int
+	UserName string
+	Message  string
+	Likes    int
+	Dislikes int
+	Date     string
+	Avatar   string
+}
+
+type OutputPost struct {
+	TabComment      []Comment
+	ID              int
+	UserID          int
+	title           string
+	PostName        string
+	Category        []string
+	PostDate        time.Time
+	UserName        string
+	PostDescription string
+	Avatar          string
+	Likes           int
+	Dislikes        int
+}
+
+type Out struct {
+	TabList      []Post
+	CategoryList []Category
+}
+
+type Post struct {
+	UserID          int
+	UserName        string
+	UserAvatar      string
+	TabComment      []Comment
+	PostName        string
+	PostCategory    string
+	PostDate        time.Time
+	PostDateString  string
+	PostDescription string
+	PostLikes       int
+	PostDislikes    int
+}
+
+type Category struct {
+	CategoryName   string
+	CategoryNumber int
+}
