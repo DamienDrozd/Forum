2790801f69fa5c3c174d02614cb0196301aa3da5
Kevin Alves <kevinalves@hotmail.fr>
2021-06-09 23:56:01 +0200

    MAJ FrontEnd 09/06/21  Post, User

--- static/js/newpost.js
+++ static/js/newpost.js
@@ -5,24 +5,24 @@
 --------------------------------------------------
 */
 function checkCookies() {
-    var btnDeconnexion = document.getElementById("Deconnexion");
-    var btnUser = document.getElementById("UserName");
-    var btnConnexion = document.getElementById("Connexion");
-    var btnInscription = document.getElementById("Inscription");
-    var cookies = document.cookie;
-    console.log("La liste des cookies :", cookies)
-  
-    if (cookies == "") {
-      btnConnexion.style.display = "block";
-      btnInscription.style.display = "block";
-      btnDeconnexion.style.display = "none";
-      btnUser.style.display = "none";
-    } else {
-      btnConnexion.style.display = "none";
-      btnInscription.style.display = "none";
-      btnDeconnexion.style.display = "block";
-      btnUser.style.display = "block";
-    }
+  var btnDeconnexion = document.getElementById("Deconnexion");
+  var btnUser = document.getElementById("UserName");
+  var btnConnexion = document.getElementById("Connexion");
+  var btnInscription = document.getElementById("Inscription");
+  var cookies = document.cookie;
+  console.log("La liste des cookies :", cookies)
+
+  if (cookies == "") {
+    btnConnexion.style.display = "block";
+    btnInscription.style.display = "block";
+    btnDeconnexion.style.display = "none";
+    btnUser.style.display = "none";
+  } else {
+    btnConnexion.style.display = "none";
+    btnInscription.style.display = "none";
+    btnDeconnexion.style.display = "block";
+    btnUser.style.display = "block";
+  }
 }  
 window.onload = checkCookies();
   
