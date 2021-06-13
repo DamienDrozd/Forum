f42a4e8381096c61efd1a554f63707fd3581b380
Kevin Alves <kevinalves@hotmail.fr>
2021-06-13 23:06:32 +0200

    MAJ Readme 13/06/21

--- static/js/index.js
+++ static/js/index.js
@@ -52,7 +52,7 @@ window.onload = checkCookies();
 */
 
 function random_bg_color() {
-  for (let id = 1; id < 11 ;id++) {
+  for (let id = 1; id < 30 ;id++) {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
